266c0931df8f26400c11297aaa21bbfd939490f1
Mark Barr <mark@hyperingenuity.com>
2018-07-25 18:58:49 -0400

    Initial generator code.

---
+++ index.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const barrkeep = require('barrkeep');
+const perlin = require('perlin-noise');
+
+////////////////////
+
+let width = 80;
+let height = 24;
+
+if (process.stdout.isTTY) {
+  width = process.stdout.columns;
+  height = process.stdout.rows;
+}
+
+const noise = perlin.generatePerlinNoise(width, height, {
+  octaveCount: 4
+});
+
+////////////////////
+
+const symbols = '░▒▓█';
+const colors = [
+  'NavyBlue',
+  'DarkBlue',
+  'DarkBlue',
+  'Blue3',
+  'Blue3',
+  'Blue1',
+  'Blue1',
+  'DodgerBlue1',
+  'DodgerBlue1',
+  'DeepSkyBlue1',
+  'DarkKhaki',
+  'DarkKhaki',
+  'Green4',
+  'Green4',
+  'Green4',
+  'Grey35',
+  'Grey50',
+  'Grey85',
+  'Grey93',
+  'White'
+];
+
+////////////////////
+
+for (let y = 0; y < height; y++) {
+  let line = '';
+  for (let x = 0; x < width; x++) {
+    const value = noise[(y * width) + x];
+    const symbol = symbols[Math.floor(value * symbols.length)];
+    const color = colors[Math.floor(value * colors.length)];
+
+    line += barrkeep.style(symbol, color);
+  }
+  console.log(line);
+}
