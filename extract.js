b7d133064a416bf8287480237a2ca1744ff8228e
Mark Barr <mark@hyperingenuity.com>
2018-07-27 10:30:52 -0400

    Automatic palette parsing and use with --palette.

--- extract.js
+++ extract.js
@@ -3,41 +3,16 @@
 
 'use strict';
 
-require('barrkeep');
-const fs = require('fs');
-const PNG = require('pngjs').PNG;
 const minimist = require('minimist');
-
+const extractor = require('./extractor');
 const options = minimist(process.argv.slice(2));
 
-const filename = options._;
-const data = fs.readFileSync(filename);
-const png = PNG.sync.read(data);
-
-function rgbToHex(rgb) {
-  return '#' + rgb.map(x => x.toString(16).padStart(2, '0')).join('');
+const filename = options._[0];
+if (!filename) {
+  console.log('Usage: ./extract.js palette.png');
+  process.exit(1);
 }
 
-const colors = [];
-
-for (let x = 0; x < png.width * 4; x += 4) {
-  const color = [
-    png.data[x],
-    png.data[x + 1],
-    png.data[x + 2]
-  ];
-
-  if (png.data[x + 3] !== 255) {
-    console.log('fail');
-  }
-
-  const hex = rgbToHex(color);
-
-  if (options.hex) {
-    colors.push(hex);
-  } else {
-    colors.push(color);
-  }
-}
+const colors = extractor(filename, options.hex);
 
 console.log(colors);
---
+++ extractor.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const fs = require('fs');
+const PNG = require('pngjs').PNG;
+
+function rgbToHex(rgb) {
+  return '#' + rgb.map(x => x.toString(16).padStart(2, '0')).join('');
+}
+
+module.exports = function(filename, hexMode) {
+  const data = fs.readFileSync(filename);
+  const png = PNG.sync.read(data);
+
+  const colors = [];
+
+  for (let x = 0; x < png.width * 4; x += 4) {
+    const color = [
+      png.data[x],
+      png.data[x + 1],
+      png.data[x + 2]
+    ];
+
+    if (png.data[x + 3] !== 255) {
+      console.log('Unhandled alpha channel');
+    }
+
+    if (hexMode) {
+      const hex = rgbToHex(color);
+      colors.push(hex);
+    } else {
+      colors.push(color);
+    }
+  }
+
+  return colors;
+};
